021757b5c1b1c86ac6b6a9259e4f067c96c3a9b2
Luisfernando2027 <feeehpz@gmail.com>
2026-06-08 21:57:04 -0300

    fix(obs): disable AddOpenTelemetryTracing in ConsolidatedService to avoid build-time extension error

--- src/ConsolidatedService/Program.cs
+++ src/ConsolidatedService/Program.cs
@@ -3,12 +3,19 @@ using MassTransit;
 using Microsoft.EntityFrameworkCore;
 using MerchantCashFlow.Contracts;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using OpenTelemetry.Trace;
+using OpenTelemetry.Resources;
 
 var builder = global::Microsoft.Extensions.Hosting.Host.CreateDefaultBuilder(args)
+    .UseSerilog((context, cfg) => cfg.WriteTo.Console())
     .ConfigureServices((context, services) =>
     {
         var conn = context.Configuration.GetConnectionString("Postgres") ?? context.Configuration["ConnectionStrings:Postgres"];
         services.AddDbContext<ConsolidatedDbContext>(opt => opt.UseNpgsql(conn));
+
+        // OpenTelemetry Tracing temporarily disabled here to avoid build-time extension mismatch in container.
+        // It is configured for LaunchesService; we can reintroduce tracing here if needed after aligning packages.
 
         services.AddMassTransit(x =>
         {
